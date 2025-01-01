13ced461c67d63cbe6b84bded1421304db6c1630
Bikash2448 <bikashcse12448@gmail.com>
2025-01-02 00:23:17 +0530

    cors add

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const http = require('http')
 const {Server} = require('socket.io')
+const cors = require('cors');
 
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST'], // Allow specific HTTP methods
+}));
 
 
 // serve the static files from the "public" folder
